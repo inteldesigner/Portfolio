e4553c0355c2084a4de6891a9ab17766a9f82fd5
IntelDesigner <ericstein.rl@gmail.com>
2018-04-17 18:02:23 -0500

    Orbit Animation html

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+// Script.js
+
+var currentDate = new Date();
+
+window.alert(currentDate);
+
+
+// if/ Else
+// var age = 50;
+            
+// f(age <= 30) {
+//     document.getElementById("box1").innerHTML = "You're just a youngin'";
+//     document.getElementById("box1").style.color = "red";
+// } else if(age >= 50) {
+//     document.getElementById("box1").innerHTML = "You are older than 50.";
+//     document.getElementById("box1").style.color = "blue";
+// } else {
+//     document.getElementById("box1").innerHTML = "You must be between 30 and 50 years old.";
+//     document.getElementById("box1").style.color = "green";
+// }
+
+body {
+    background: #333 url() center center no-repeat;
+    background-size
+}
